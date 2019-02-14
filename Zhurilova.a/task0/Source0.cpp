99afdd98f31ee21eb142dbdc042337913365e04c
Журилова Анастасия Сергеевна <zhurilova.a@ccam.cmc.unn.net>
2019-02-14 10:11:55 +0300

    Незаконченное

---
+++ Zhurilova.a/task0/Source0.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <locale.h>
+#include <ctime>
+
+class mytime
+{
+private:
+	clock_t T;
+public:
+	int Set_time(mytime T)
+	{
+		
+
+	}
+
+};
+
+void main()
+{
+	int v = -1;
+	mytime T;
+	printf("Что сделать: 1)Установить время;/n 2)Узнать время;/n 3)Вычислить разницу;/n 4)Сдвинуть время;/n");
+	scanf("%d", &v);
+	if (v == 1) {
+		printf("Установка времени/n");
+		
+		T.Set_time;
+	}		
+	if (v == 2) {}
+	if (v == 3) {}
+	if (v == 4) {}
+	
+
+	
+
+
+}
