e32ce807721215e19af9e0d5f432de8e633608dd
Zhurilovaa <Anastasiya.Zhurilova@yandex.ru>
2019-03-06 19:25:25 +0300

    Исправила замечания

--- Zhurilova.a/task1/Source1.cpp
+++ Zhurilova.a/task1/Source1.cpp
@@ -31,7 +31,7 @@ public:
 		{
 			for (int j = 0; j < n; j++)
 			{
-				printf("%d", matrix[i][j]);
+				printf("%d  ", matrix[i][j]);
 			}
 			printf("\n");
 		}
@@ -44,11 +44,12 @@ public:
 		}
 		else if ((_n < 2) || (n > 8)) {
 			printf("Неверно задан размер\n");
-		}			 
-	}	
-	void Print_Size() const//Вывод размера матрицы
+		}
+	}
+	int Print_Size(int &N)//Узнать размер матрицы
 	{
-		printf("Размер матрицы %d", n);
+		N = n;
+		return N;
 	}
 	void Set_element(int a, int i, int j)//Установка элемента
 	{
@@ -58,18 +59,15 @@ public:
 		}
 		else printf("Нет элемента с таким индексом в матрице\n");
 	}
-	void Print_element(int i, int j) const//Вывод элемента
+	int Print_element(int &i, int &j, int &elem)//Узнать элемент матрицы по индексам
 	{
-		if ((i >= 0) && (i < n) && (j >= 0) && (j < n))
-		{
-			printf("Элемент под таким индексом %d", matrix[i][j]);
-		}
-		else printf("Нет элемента под таким индексом\n");
+		elem = matrix[i][j];
+		return elem;
 	}
-	void Diagonal_matrix()//Проверка на диагональное преобладание
+	bool Diagonal_matrix(bool &diag)//Проверка на диагональное преобладание
 	{
-		int i, j, sum=0;
-		for (i = 0;i < n; i++)
+		int i, j, sum = 0;
+		for (i = 0; i < n; i++)
 		{
 			for (j = 1; j < n; j++)
 			{
@@ -77,16 +75,13 @@ public:
 			}
 			if (matrix[i][i] >= sum)
 			{
-				Diagonal= true;
-			}			
+				Diagonal = true;
+			}
 		}
-		if (Diagonal == true)
-		{
-			printf("Матрица облидает диагональным преобладанием\n");
-		}
-		else printf("Матрица не обладет диагональным преобладанием\n");
+		diag = Diagonal;
+		return diag;
 	}
-	Matrix Add(Matrix _m)//Сложение матриц
+	Matrix Add(const Matrix &_m)//Сложение матриц
 	{
 		if (n == _m.n)
 		{
@@ -94,12 +89,12 @@ public:
 			{
 				for (int j = 0; j < n; j++)
 				{
-					_m.matrix[i][j] = _m.matrix[i][j] + matrix[i][j];
+					matrix[i][j] = _m.matrix[i][j] + matrix[i][j];
 				}
 			}
 		}
 		else printf("У матриц не совпадает размер\n");
-		return _m;
+		return *this;
 	}
 	Matrix& operator=(const Matrix &m)//Оператор присваивания
 	{
@@ -113,21 +108,14 @@ public:
 		}
 		return *this;
 	}
-	~Matrix()//Деструктор
-	{
-		for (int i = 0; i < n; i++)
-		{
-			delete matrix[i];			
-		}
-		delete[] matrix;
-	}
 };
 void main()
 {
 	setlocale(LC_ALL, "Rus");
 	Matrix M1, M2, M3;
-	int elem[8][8] = {1};
+	int elem;
 	int N, N1;
+	bool diag=false;
 	int exit=0,v = 0,i,j;
 	while (exit == 0)
 	{
@@ -149,9 +137,9 @@ void main()
 			}
 			M1.Set_Size(N);
 		}
-		if (v == 2) {
-			printf("Размер матрицы\n");
-			M1.Print_Size();
+		if (v == 2) {			
+			M1.Print_Size(N);
+			printf("Размер матрицы %d\n", N);
 		}
 		if (v == 3) {
 			printf("Введите номер строки и столбца нужного элемента\n");
@@ -162,8 +150,8 @@ void main()
 				scanf_s("%d %d", &i, &j);
 			}
 			printf("Введите значение элемента матрицы(целое число)\n");
-			scanf_s("%d", &elem[i][j]);
-			M1.Set_element(elem[i][j], i, j);
+			scanf_s("%d", &elem);
+			M1.Set_element(elem, i, j);
 		}
 		if (v == 4) {
 			printf("Введите номер строки и столбца нужного элемента\n");
@@ -173,11 +161,17 @@ void main()
 				printf("Нет элемента с таким индексом\n");
 				scanf_s("%d %d", &i, &j);
 			}
-			M1.Print_element(i, j);
+			M1.Print_element(i, j, elem);
+			printf("Элемент под таким индексом %d",elem);
 		}
 		if (v == 5) {
 			printf("Обладает ли матрица диагональным преобладанием?\n");
-			M1.Diagonal_matrix();
+			M1.Diagonal_matrix(diag);
+			if (diag == true)
+			{
+				printf("Матрица облидает диагональным преобладанием\n");
+			}
+			else printf("Матрица не обладет диагональным преобладанием\n");
 		}
 		if(v==6){
 			printf("Сложение матриц\n");
